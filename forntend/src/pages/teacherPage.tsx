3c56541289b66e8bdb726273cc0c4365bb8af7bc
Eyob <eyobbirhanu111000@gmail.com>
2026-03-02 12:55:05 +0300

    feat: enhance TeacherPage with chart configuration and data integration for ChartAreaInteractive

--- forntend/src/pages/teacherPage.tsx
+++ forntend/src/pages/teacherPage.tsx
@@ -1,15 +1,32 @@
 import { ChartAreaInteractive } from "@/components/chart-area-interactive"
 import { DataTable } from "@/components/data-table"
 import { SectionCards } from "@/components/section-cards"
-
+import type { ChartConfig } from "@/components/ui/chart";
 import data from "./data.json"
+import chartData from "./sampleDatas/chartData.json";
+
+const chartConfig = {
+  visitors: {
+    label: "Visitors",
+  },
+  desktop: {
+    label: "Desktop",
+    color: "var(--primary)",
+  },
+
+} satisfies ChartConfig
+
+const chartDataConfig = {
+  xAxis: "date",
+  area: ["timeSpent"],
+};
 
 export default function TeacherPage() {
   return (
     <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
         <SectionCards />
         <div className="px-4 lg:px-6">
-          <ChartAreaInteractive />
+          <ChartAreaInteractive chartConfig={chartConfig} chartDataConfig={chartDataConfig} chartData={chartData} />
         </div>
         <DataTable data={data} />
     </div>
